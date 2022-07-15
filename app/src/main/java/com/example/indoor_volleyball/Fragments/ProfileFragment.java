f673f48120e696b1e35051114ac2a4415cb1a810
nabarnes43 <89264306+nabarnes43@users.noreply.github.com>
2022-07-14 18:01:51 -0700

    Profile camera and format

--- app/src/main/java/com/example/indoor_volleyball/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/indoor_volleyball/Fragments/ProfileFragment.java
@@ -1,9 +1,16 @@
 package com.example.indoor_volleyball.Fragments;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,19 +22,32 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.example.indoor_volleyball.Activities.CreateEventActivity;
 import com.example.indoor_volleyball.Activities.CreateGymActivity;
 import com.example.indoor_volleyball.Activities.LoginActivity;
 import com.example.indoor_volleyball.Activities.QueryActivity;
 import com.example.indoor_volleyball.databinding.FragmentProfileBinding;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
+
+import java.io.File;
+import java.util.Objects;
 
 
 public class ProfileFragment extends Fragment {
     private static final String TAG = "ProfileFragment";
+    private ParseUser user;
     private FragmentProfileBinding binding;
+    public File photoFile;
+    public String photoFileName = "photo.jpg";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
     ActivityResultLauncher<Void> EventCreator = registerForActivityResult(new CreateEvent(),
             new ActivityResultCallback<Boolean>() {
                 @Override
@@ -75,11 +95,14 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.btCreateEvent.setOnClickListener(new View.OnClickListener() {
+        user = ParseUser.getCurrentUser();
+        displayUserInfo();
+
+
+        binding.btTakePhotoProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getContext(), CreateEventActivity.class);
-                EventCreator.launch(null);
+                launchCamera();
             }
         });
 
@@ -112,6 +135,69 @@ public class ProfileFragment extends Fragment {
         }));
     }
 
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(TAG, "failed to create directory");
+        }
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+    }
+
+    public void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(requireContext().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+
+    public void displayUserInfo() {
+        binding.tvUsername.setText(user.getUsername());
+        ParseFile profilePhoto = user.getParseFile("profilePhoto");
+        if (profilePhoto != null) {
+            Glide.with(requireContext()).load(user.getParseFile("profilePhoto").getUrl()).circleCrop().into(binding.ivProfilePhoto);
+        } else {
+            Toast.makeText(getContext(), "this Profile photo does not exist for user " + user.getUsername(), Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                binding.ivProfilePhoto.setImageBitmap(takenImage);
+                Glide.with(requireContext()).load(takenImage).circleCrop().into(binding.ivProfilePhoto);
+                user.put("profilePhoto",  new ParseFile(photoFile));
+                user.saveInBackground();
+            } else { // Result was a failure
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     public class CreateEvent extends ActivityResultContract<Void, Boolean> {
         @NonNull
         @Override
@@ -122,7 +208,7 @@ public class ProfileFragment extends Fragment {
 
         @Override
         public Boolean parseResult(int resultCode, @Nullable Intent result) {
-            return resultCode == Activity.RESULT_OK;
+            return resultCode == RESULT_OK;
         }
     }
 
@@ -136,7 +222,7 @@ public class ProfileFragment extends Fragment {
 
         @Override
         public Boolean parseResult(int resultCode, @Nullable Intent result) {
-            return resultCode == Activity.RESULT_OK;
+            return resultCode == RESULT_OK;
         }
     }
 
@@ -150,7 +236,7 @@ public class ProfileFragment extends Fragment {
 
         @Override
         public Boolean parseResult(int resultCode, @Nullable Intent result) {
-            return resultCode == Activity.RESULT_OK;
+            return resultCode == RESULT_OK;
         }
     }
 
@@ -164,7 +250,7 @@ public class ProfileFragment extends Fragment {
 
         @Override
         public Boolean parseResult(int resultCode, @Nullable Intent result) {
-            return resultCode == Activity.RESULT_OK;
+            return resultCode == RESULT_OK;
         }
     }
 }
